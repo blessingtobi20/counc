da4b7d2099afd14a11cc9046276f5b869736aaa7
blessingtobi20 <115149530+blessingtobi20@users.noreply.github.com>
2023-01-08 18:41:06 +0100

    js

--- js/main.js
+++ js/main.js
@@ -76,6 +76,23 @@
 
     
 })(jQuery);
+var thehours = new Date().getHours();
+	var themessage;
+	var morning = ('Good morning');
+	var afternoon = ('Good afternoon');
+	var evening = ('Good evening');
+
+	if (thehours >= 0 && thehours < 12) {
+		themessage = morning; 
+
+	} else if (thehours >= 12 && thehours < 17) {
+		themessage = afternoon;
+
+	} else if (thehours >= 17 && thehours < 24) {
+		themessage = evening;
+	}
+
+	$('.greeting').append(themessage);
 
 // const form = document.getElementById('form');
 // const result = document.getElementById('result');
