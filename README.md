a67b27f6b4d5ee02e65123421f883aa9baff2957
blessingtobi20 <115149530+blessingtobi20@users.noreply.github.com>
2023-01-08 09:34:48 +0100

    new

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# counc
+# LUSC
+ LUSC
---
+++ js/main.js
@@ -0,0 +1,121 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+    
+    
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('active');
+        $(this).addClass('active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText : [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+    
+})(jQuery);
+
+// const form = document.getElementById('form');
+// const result = document.getElementById('result');
+
+// form.addEventListener('submit', function(e) {
+//     const formData = new FormData(form);
+//     e.preventDefault();
+//     var object = {};
+//     formData.forEach((value, key) => {
+//         object[key] = value
+//     });
+//     var json = JSON.stringify(object);
+//     result.innerHTML = "Please wait..."
+
+//     fetch('https://api.web3forms.com/submit', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//                 'Accept': 'application/json'
+//             },
+//             body: json
+//         })
+//         .then(async (response) => {
+//             let json = await response.json();
+//             if (response.status == 200) {
+//                 result.innerHTML = json.message;
+//             } else {
+//                 console.log(response);
+//                 result.innerHTML = json.message;
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error);
+//             result.innerHTML = "Something went wrong!";
+//         })
+//         .then(function() {
+//             form.reset();
+//             setTimeout(() => {
+//                 result.style.display = "none";
+//             }, 3000);
+//         });
+// });
+
